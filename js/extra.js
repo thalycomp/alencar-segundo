bb652e2846d6f6824985d22d564ee1be506f9abb
thalycomp <thalycomp@gmail.com>
2020-10-30 13:59:58 -0300

    transiçao suave botao flutuante

---
+++ js/extra.js
@@ -0,0 +1,16 @@
+function ativaNoScroll() {
+  const botaoFlutuante = document.querySelector('.up-to-page');
+  const proxSection = document.querySelector('.esc');
+
+    if(document.querySelector('.esc').getBoundingClientRect().height < window.scrollY) {
+          
+          botaoFlutuante.style.display = 'block';
+
+    } else {
+          
+          botaoFlutuante.style.display = 'none';
+    }
+
+}
+
+window.addEventListener('scroll', ativaNoScroll);
